b6c40fd670f0ebf300c7492f55dea87bfbeca3ce
Lukas Forst <lukas.forst@zoho.com>
2018-11-02 19:17:59 +0100

    Fix non-latex html entities, extend contents

--- src/contentdb.js
+++ src/contentdb.js
@@ -6,32 +6,32 @@ const contentdb = [
 		["el. Stromstärke", "I", "1 A \\, \\textrm{(Ampere)}"],
 		["absolute Temperatur", "T", "1 K \\, \\textrm{(Kelvin)}"],
 		["Stoffmenge", "n", "1 \\, mol"],
-		["Lichtstärke", "I<sub>V</sub>", "1 \\, cd \\, \\textrm{(Candela)}"],
-		["Lichtgeschwindigkeit", "c<sub>0</sub>", "299.792.458 \\frac{m}{s} \\approx 300.000 \\frac{km}{s}"],
+		["Lichtstärke", "I_V", "1 \\, cd \\, \\textrm{(Candela)}"],
+		["Lichtgeschwindigkeit", "c_0", "299.792.458 \\frac{m}{s} \\approx 300.000 \\frac{km}{s}"],
 	/* Wärmelehre */
 		["absolute Temperatur", "T", "1 K \\, \\textrm{(Kelvin)}"],
-		["Celsius-Temperatur", "&theta;, &#977;", "1 °C \\, \\textrm{(Grad Celsius)}"],
+		["Celsius-Temperatur", "\\theta, \\, \\vartheta", "1 °C \\, \\textrm{(Grad Celsius)}"],
 		["Leistung, Energiestrom", "P", "1 \\frac{J}{s} = 1 V \\cdot A = 1 W \\textrm{(Watt)}"],
 		["Energie", "E", "1 J \\, \\textrm{(Joule)}"],
 		["Wärme", "Q", "1 J \\, \\textrm{(Joule)}"],
 		["spezifische Wärmekapazität", "c", "1 \\frac{J}{kg \\cdot K}"],
-		["Arbeit", "&Delta;E, W", "1 1 J \\, \\textrm{(Joule)}"],
+		["Arbeit", "\\Delta E, \\, W", "1 J \\, \\textrm{(Joule)}"],
 		["Wirkungsgrad", "&eta;", "1 = 100% \\, \\textrm{(dimensionslose Dezimalzahl < 1)"],
 	/* Mechanik */
 		["Geschwindigkeit", "v", "1 \\frac{m}{s}"],
 		["Impuls", "\\vec{p}", "\\frac{kg \\cdot m}{s}"],
-		["Kraft", "F", "1 N \\, \\textrm{(Newton)}"],
+		["Kraft", "\\vec{F}", "1 N \\, \\textrm{(Newton)}"],
 		["Erd- / Fallbeschleunigung", "g", "9,81\\frac{m}{s^2} = 9,81 \\frac{N}{kg}"],
-		["Beschleunigung", "a", "1 \\frac{m}{s^2}"],
+		["Beschleunigung", "\\vec{a}", "1 \\frac{m}{s^2}"],
 		["Druck", "p", "1 \\frac{N}{m^2} = 1 Pa \\, \\textrm{(Pascal)}"],
 		["Dichte", "&rho;", "1 \\frac{kg}{m^3}"],
 		["Federkonstante", "D", "1 \\frac{N}{m}"],
-		["Reibungskoeffizient", "&mu;", "1 \\, \\textrm{(dimensionslose Dezimalzahl)}"],
+		["Reibungskoeffizient", "\\mu", "1 \\, \\textrm{(dimensionslose Dezimalzahl)}"],
 	/* Elektrizitätslehre */
 		["el. Stromstärke", "I", "1 A \\, \\textrm{(Ampere)}"],
 		["el. Widerstand", "R", "1 \\Omega \\, \\textrm{(Ohm)}"],
 		["el. Spannung", "U", "1 V \\, \\textrm{(Volt)}"],
-		["el. Potential", "&Phi;, &phi;, &#981;", "1 V \\, \\textrm{(Volt)}"],
+		["el. Potential", "\\Phi, \\phi", "1 V \\, \\textrm{(Volt)}"],
 		["el. Ladung", "Q", "1 C \\, \\textrm{(Coulomb)}"],
 		["Lorentzkraft", "F<sub>L</sub>", "1 N \\, \\textrm{(Newton)}"],
 	/* Atom- und Kernphysik */
@@ -44,7 +44,7 @@ const contentdb = [
 		["Nukleonenzahl, Massenzahl", "A", "1 \\, \\textrm{dimensionslose Anzahl}"],
 	/* Akustik */
 		["Frequenz", "f", "\\frac{1}{s} = 1 Hz \\, \\textrm{(Hertz)}"],
-		["Schalldruckpegel", "L<sub>p</sub>", "1 dB \\, \\textrm{(Dezibel)} = 0,1 B \\, \\textrm{(Bel)}"],
+		["Schalldruckpegel", "L_p", "1 dB \\, \\textrm{(Dezibel)} = 0,1 B \\, \\textrm{(Bel)}"],
 	/* Optik */
 		["Brennweite", "f", "1 m"],
 		["Brechwert, Brechkraft", "D", "\\frac{1}{m} = 1 dpt \\, \\textrm{(Dioptrie)}"],
@@ -52,7 +52,10 @@ const contentdb = [
 		["Bildweite", "b", "1 m"],
 		["Gegenstandsgröße", "G", "1 m"],
 		["Gegenstandsweite", "g", "1 m"],
-		["Wellenlänge", "&lambda;", "1 m"]
+		["Wellenlänge", "&lambda;", "1 m"],
+	/* Elektrische und magnetische Felder */
+		["Magnetische Flussdichte", "\\vec{B}", "1 T \\, \\textrm{Tesla}"],
+		["el. Feldstärke", "\\vec{E}", "1 \\frac{N}{C} = 1 \\frac{V}{m}"]
 ];
 
 export { contentdb };

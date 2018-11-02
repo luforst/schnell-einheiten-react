596b618d9d79833e3855f2eace072fc491392f1a
Lukas Forst <lukas.forst@zoho.com>
2018-11-02 18:53:16 +0100

    Fix missing commas

--- src/contentdb.js
+++ src/contentdb.js
@@ -14,9 +14,9 @@ const contentdb = [
 		["Leistung, Energiestrom", "P", "1 \\frac{J}{s} = 1 V \\cdot A = 1 W \\textrm{(Watt)}"],
 		["Energie", "E", "1 J \\, \\textrm{(Joule)}"],
 		["Wärme", "Q", "1 J \\, \\textrm{(Joule)}"],
-		["spezifische Wärmekapazität", "c", "1 \\frac{J}{kg \\cdot K}"]
+		["spezifische Wärmekapazität", "c", "1 \\frac{J}{kg \\cdot K}"],
 		["Arbeit", "&Delta;E, W", "1 1 J \\, \\textrm{(Joule)}"],
-		["Wirkungsgrad", "&eta;", "1 = 100% \\, \\textrm{(dimensionslose Dezimalzahl < 1)"]
+		["Wirkungsgrad", "&eta;", "1 = 100% \\, \\textrm{(dimensionslose Dezimalzahl < 1)"],
 	/* Mechanik */
 		["Geschwindigkeit", "v", "1 \\frac{m}{s}"],
 		["Impuls", "\\vec{p}", "\\frac{kg \\cdot m}{s}"],
@@ -26,14 +26,14 @@ const contentdb = [
 		["Druck", "p", "1 \\frac{N}{m^2} = 1 Pa \\, \\textrm{(Pascal)}"],
 		["Dichte", "&rho;", "1 \\frac{kg}{m^3}"],
 		["Federkonstante", "D", "1 \\frac{N}{m}"],
-		["Reibungskoeffizient", "&mu;", "1 \\, \\textrm{(dimensionslose Dezimalzahl)}"]
+		["Reibungskoeffizient", "&mu;", "1 \\, \\textrm{(dimensionslose Dezimalzahl)}"],
 	/* Elektrizitätslehre */
 		["el. Stromstärke", "I", "1 A \\, \\textrm{(Ampere)}"],
 		["el. Widerstand", "R", "1 \\Omega \\, \\textrm{(Ohm)}"],
 		["el. Spannung", "U", "1 V \\, \\textrm{(Volt)}"],
 		["el. Potential", "&Phi;, &phi;, &#981;", "1 V \\, \\textrm{(Volt)}"],
 		["el. Ladung", "Q", "1 C \\, \\textrm{(Coulomb)}"],
-		["Lorentzkraft", "F<sub>L</sub>", "1 N \\, \\textrm{(Newton)}"]
+		["Lorentzkraft", "F<sub>L</sub>", "1 N \\, \\textrm{(Newton)}"],
 	/* Atom- und Kernphysik */
 		["Aktivität", "A", "1 Bq \\, \\textrm{(Becquerel)}"],
 		["Energiedosis", "D", "1 Gy \\, \\textrm{(Gray)}"],
@@ -41,10 +41,10 @@ const contentdb = [
 		["Ionendosis", "J", "1 \\frac{A \\cdot s}{kg} = 1 \\frac{Q}{kg} = 1 R \\, \\textrm{(Röntgen)}"],
 		["Ordnungs-, Kernladungs- oder Protonenzahl", "Z", "1 \\, \\textrm{dimensionslose Anzahl}"],
 		["Neutronenzahl", "N", "1 \\, \\textrm{dimensionslose Anzahl}"],
-		["Nukleonenzahl, Massenzahl", "A", "1 \\, \\textrm{dimensionslose Anzahl}"]
+		["Nukleonenzahl, Massenzahl", "A", "1 \\, \\textrm{dimensionslose Anzahl}"],
 	/* Akustik */
 		["Frequenz", "f", "\\frac{1}{s} = 1 Hz \\, \\textrm{(Hertz)}"],
-		["Schalldruckpegel", "L<sub>p</sub>", "1 dB \\, \\textrm{(Dezibel)} = 0,1 B \\, \\textrm{(Bel)}"]
+		["Schalldruckpegel", "L<sub>p</sub>", "1 dB \\, \\textrm{(Dezibel)} = 0,1 B \\, \\textrm{(Bel)}"],
 	/* Optik */
 		["Brennweite", "f", "1 m"],
 		["Brechwert, Brechkraft", "D", "\\frac{1}{m} = 1 dpt \\, \\textrm{(Dioptrie)}"],
